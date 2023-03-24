042f26ca346493dac81ff699e45a04b6f4302976
Gustaw Tomkowski <gustawspowerfulmind@Gustaws-MacBook-Air.local>
2023-03-24 21:22:03 +0100

    created not fully functional modal toggle feature

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Modal from "./components/Modal/Modal";
 function App() {
 	const [itemArray, setItemArray] = useState([]);
 	const [inputObject, setInputObject] = useState({});
-	const [isClicked, setIsClicked] = useState(false);
-
+	const [showModal, setShowModal] = useState(false);
 	const handleListData = (listItem) => {
-		setItemArray((prevExpenses) => {
-			return [listItem, ...prevExpenses];
-		});
+		if (listItem.username !== "" && listItem.age !== "" && listItem.age > 0) {
+			setItemArray((prevExpenses) => {
+				return [listItem, ...prevExpenses];
+			});
+		}
 	};
 
 	const handleInputData = (inputData) => {
 		setInputObject(inputData);
 	};
 
+	// showModal needs to be triggered to true
+
 	const nonNegativeMessage =
 		"Please enter a valid name and age (non-empty values)";
 	const greaterThanZeroMessage = `Please enter a valid age (> 0)`;
 	let modalContent = null;
-
 	if (inputObject.username === "" || inputObject.age === "") {
 		modalContent = <Modal message={nonNegativeMessage}></Modal>;
 	}
@@ -34,11 +36,10 @@ function App() {
 		<div className={styles.wrapper}>
 			<InputForm onSaveArray={handleListData} inputData={handleInputData} />
 			{modalContent}
-			{modalContent === null && <NameList passedArray={itemArray}></NameList>}
+
+			<NameList passedArray={itemArray}></NameList>
 		</div>
 	);
 }
 
 export default App;
-
-// teraz jeszcze tylko zostalo mi zamkniecie formularza
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Modal.module.css";
 
 const Modal = (props) => {
-	// nawet jak zmienimy wartosc z powrotem na null, 
-
+	const [modal, setModal] = useState(true);
+	const clickHandler = () => {
+		setModal(!modal);
+	};
 	return (
-		<div className={styles.modal}>
-			<div className={styles["modal__header"]}>
-				<h2 className={styles["modal__title"]}>Invalid Input</h2>
-			</div>
-			<div className={styles["modal__body"]}>
-				<p className={styles["modal__text"]}>{props.message}</p>
-				<button className={styles["modal__button"]}>okay</button>
-			</div>
+		<div>
+			{modal && (
+				<div>
+					<div className={styles.overlay}></div>
+					<div className={styles.modal}>
+						<div className={styles["modal__header"]}>
+							<h2 className={styles["modal__title"]}>Invalid Input</h2>
+						</div>
+						<div className={styles["modal__body"]}>
+							<p className={styles["modal__text"]}>{props.message}</p>
+							<button
+								onClick={clickHandler}
+								className={styles["modal__button"]}
+							>
+								okay
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
+// upon clicking on the button I want to disable the component somewhere else
 
+// i can move state from modal to app and then depending on that state I can conditionally render a modal
 export default Modal;
-
-
